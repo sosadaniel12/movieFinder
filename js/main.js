8357641edca54e41732c90af41a1518d368bbb4c
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-27 10:42:44 -0500

    fixed the button search

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 $(document).ready(function () {
-  $("#searchForm").on("submit", (e) => {
+  $("#searchButton").click("submit", (e) => {
     let searchText = $("#searchText").val();
     getMovies(searchText);
     e.preventDefault();
   });
 });
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 function getMovies(searchText) {
   axios
